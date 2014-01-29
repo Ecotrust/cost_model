7b63f91975ea53885a1bd6d5de9ae7e004d7304b
Matthew Perry <perrygeo@gmail.com>
2014-01-29 12:43:32 -0800

    scripts for cost model validation

---
+++ scripts/validate.py
@@ -0,0 +1,51 @@
+from forestcost import main_model
+import random
+import csv
+
+costdata = csv.DictReader(open('cost_data.csv','rb'))
+
+fields = """acres elev slope stand_wkt_dummy RemovalsCT TreeVolCT
+          RemovalsSLT TreeVolSLT RemovalsLLT TreeVolLLT
+          HdwdFractionCT HdwdFractionSLT HdwdFractionLLT
+          PartialCut landing_coords_dummy haulDist haulTime coord_mill_dummy""".split()
+          
+          # Helicopter = True 
+          # HaulProportion = 1.0
+          # skid_distance=None
+
+iters = 200
+
+print "name,actual,predicted\n"
+for row in costdata:
+    for i in range(iters):
+        args = []
+        for field in fields:
+            val = row[field]
+            try:
+                val = float(val)
+            except ValueError:
+                if "|" in val:
+                    # randomly sample from uniform distribution
+                    low, high = [float(x) for x in val.split("|")]
+                    val = random.uniform(low, high)
+
+            args.append(val)
+
+        if row['PartialCut'] == 1:
+            haulprop = 0.5
+        else:
+            haulprop = 1.0
+
+        val = row['skid_distance']
+        try:
+            skid_distance = float(val)
+        except ValueError:
+            if "|" in val:
+                # randomly sample from uniform distribution
+                low, high = [float(x) for x in val.split("|")]
+                skid_distance = random.uniform(low, high)
+
+        #print args
+        results = main_model.cost_func(*args, Helicopter=False, HaulProportion=haulprop, skid_distance=skid_distance)
+        print ",".join(str(x) for x in [row['name'], row['actual_cost'], results['total_cost']])
+        print "\n"
